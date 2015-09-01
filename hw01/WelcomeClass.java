607a6bbcdfb8db4a2d42cb4c2608dce94f553c0d
Joshua Kapcsos <jok213@lehigh.edu>
2015-09-01 16:29:55 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//Joshua Kapcsos
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+    
+//  print the desired output    
+    System.out.println("  -----------"                                  );
+    System.out.println("  | WELCOME |"                                  );
+    System.out.println("  -----------"                                  );
+    System.out.println("  ^  ^  ^  ^  ^  ^"                             );
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\"                      );
+    System.out.println("<-J--O--K--2--1--3->"                           );
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / "                     );
+    System.out.println("  v  v  v  v  v  v"                             );
+    System.out.println("Born and raised in Bethlehem, PA."              );
+    System.out.println("I was a High School Scholar here two years ago,");
+    System.out.println("and now I'm here full-time!"                    );
+//  I split the autobiographical statement into three lines for aesthetic reasons.    
+  
+    }
+}
