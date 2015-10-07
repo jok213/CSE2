4f55b03c93690a5d966330b23f810110a2678ad0
Joshua Kapcsos <jok213@lehigh.edu>
2015-10-07 05:55:58 +0000

    updated lab06

--- hw06/CheckDigit.java
+++ hw06/CheckDigit.java
@@ -27,13 +27,16 @@ public class CheckDigit{
         //note: because there is no ln, the cursor will not go to the next line after printing this
     
     String barcode = myScanner.next();
-        //calls a method that Scanner objects have - "nextInt()" method
+        //calls a method that Scanner objects have - "next()" method
         //can only be accessed by telling Java that a method part of myScanner object is needed
-        //to accept an int, nextInt() method of the Scanner object will be used
-        //e.g., my Scanner.nextInt()
+        //to accept a string, next() method of the Scanner object will be used
+    while (barcode.length() != 10) {
+        System.out.print("This is a valid NOT a valid ISBN. Please enter 10 digits: ");
+        barcode = myScanner.next();
+    }    
     
     //now to declare two strings, one for the first 9 digits, and the other for the last digit:
-    String lastChar = barcode.substring(barcode.length()-1, barcode.length());
+    String checkDigit = barcode.substring(barcode.length()-1, barcode.length());
         //this string only goes between the second to last character to the end, which is the last character
     String allButLast = barcode.substring(0, barcode.length()-1);
         //this string goes from the beginning of the input to one before the last, since it needs to be separated from the check digit
@@ -44,10 +47,10 @@ public class CheckDigit{
     int holder = 0;          //holder gets the value of the rightmost integer in the number
     int sum = 0;             //the sum achieved at the end is declared at zero
     int multiChar = 0;       //multiChar will convert the X value to 10
-    for (int i=2; i>=10; i++){ //guarantees that the loop will only run 9 times
+    for (int i = 2; i >= 10; i++){ //guarantees that the loop will only run 9 times
         barcode2 = barcode1;   //stores the extra variable that comes in handy in a bit
-        if(barcode1<10){
-            sum+=barcode1*10;
+        if(barcode1 < 10){
+            sum += barcode1 * 10;
             //if the first nine digits are less than 10, it's added to the sum after being multipied by 10
             //this occurs with the leftmost integer
         }
@@ -64,24 +67,40 @@ public class CheckDigit{
         }
     }
     //now to solve the X problem:
-    if(lastChar==88){
+    char lastChar = 0;
+    int lastInt = 0;
+    lastChar = checkDigit.charAt(0);
+    lastInt = (int) lastChar;
+    if(lastInt == 88){
         multiChar = 10;
         //the value of X as a char is 88, so this converts the X value to an integer value of 10 instead
     }
-    else if(lastChar>=48&&lastChar<=57){
-        multiChar = Integer.parseInt(lastChar);
-        //if the check digit is anywhere from 0 to 9, it just parses to that integer
+    else if(lastInt >= 48 && lastInt <= 57 ){
+        multiChar = Character.getNumericValue(lastChar);
+        //if the check digit is anywhere from 0 to 9, it just casts to that integer
     }
     else{
         System.out.println("Your last character in your barcode is invalid.");
         //if the check digit is anything else, an error message occurs
+    }
+    
+    //declare variables to print what the check value should actually be:
+    String newCheck = "";    //will eventually print the new check
+    int intCheck = sum % 11; //intCheck is the remainder of the sum after dividing by 11
+    if (intCheck == 10) {
+        newCheck = "X";
+        //now if the remainder is 10, it can print X as the check digit
+    }
+    else{
+        newCheck = Integer.toString(intCheck);
+        //if the remainder is anything else, it will convert the string of that integer
     }
     if(sum % 11 == multiChar){
         System.out.println("This is a valid ISBN.");
         //if the remainder after dividing the sum by 11 equals the value of the check digit, the ISBN is stated to be valid
     }    
     else{
-        System.out.println("This is NOT a valid ISBN. Check digit should be " + (sum % 11));
+        System.out.println("This is NOT a valid ISBN. Check digit should be " + newCheck);
         //if the remainder after dividing the sum by 11 does not equal the value of the check digit, the ISBN is not valid
         //prints the statement that the check digit should equal the remainder after the sum is divided by 11
     }
