4ca0c76c8312b96599bc6cb24176c9afb02b5e81
Joshua Kapcsos <jok213@lehigh.edu>
2015-10-13 06:05:21 +0000

    updated Twisty

--- hw07/Twisty.java
+++ hw07/Twisty.java
@@ -7,6 +7,7 @@
 //Write a program that asks the user for two integers: Length and Width.
     //Length is to specify how many characters the pattern should spread, horizontally, across the screen.
     //Width should specify how many characters the pattern should spread, vertically, across the screen.
+//Creates a pattern from these inputs, as shown in the instructions
     
 import java.util.Scanner;
     //imports the scanner class for inputs
@@ -40,7 +41,7 @@ while (!acceptable) {
         //if the input is positive and no more than 80, it is accepted, and the loop is exited by changing the boolean to true
         }
         else if (length < 0 || length > 80) {
-        System.out.println("   ERROR: Your length must be from 0 to 80");
+        System.out.println("   ERROR: Your length must be from 0 to 80.");
         System.out.print("Input your desired length: ");
         continue;
         //if the input is negative or greater than 80, an error message prompts for a new input, and the loop goes back to the beginning
@@ -49,7 +50,7 @@ while (!acceptable) {
     else {
         //if not an int, trash it.  This does not assign the output of 
         //next() to anything. The output is just discarded.
-        System.out.println("   ERROR: Please type in an integer");
+        System.out.println("   ERROR: Please type in an integer.");
         System.out.print("Input your desired length: ");
         myScanner.next();
     }
@@ -76,7 +77,7 @@ while (!acceptable1) {
         //if the input is positive and less than the length, it is accepted, and the loop is exited by changing the boolean to true
         }
         else if (width < 0 || width > length) {
-        System.out.println("   ERROR: Please input an integer smaller than the length.");
+        System.out.println("   ERROR: Please input an integer no greater than that of the length.");
         System.out.print("Input your desired width: ");
         continue;
         //if the input is negative or more than the length, an error message prompts for a new input, and the loop goes back to the beginning
@@ -85,7 +86,7 @@ while (!acceptable1) {
     else {
         //if not an int, trash it.  This does not assign the output of 
         //next() to anything. The output is just discarded.
-        System.out.println("   ERROR: Please type in an integer");
+        System.out.println("   ERROR: Please type in an integer.");
         System.out.print("Input your desired width: ");
         myScanner.next();
     }
